9e70b5ad72f41a9cbe78bbc53125b1c37ad5c70e
ENRICO <fenrico472@gmail.com>
2026-03-27 11:43:40 +0100

    Aggiunto Scroll view per le notifiche
    Ora le notifiche possono esser mostrate a cascata.

--- FindingUniverseSeriousGame/Assets/Scripts/UiScripts/Notification_Manager.cs
+++ FindingUniverseSeriousGame/Assets/Scripts/UiScripts/Notification_Manager.cs
@@ -4,7 +4,9 @@ using System.Collections;
 
 public class Notification_Manager : MonoBehaviour
 {
-    [SerializeField] private Vfx_Typewriter typewriter;  // Oggetto con il testo
+    //[SerializeField] private Vfx_Typewriter typewriter;  // Oggetto con il testo
+    [SerializeField] GameObject prefabTxtNotification;
+    [SerializeField] GameObject notifPanel;
 
     private void OnEnable()
     {
@@ -15,16 +17,25 @@ public class Notification_Manager : MonoBehaviour
         DelegateClass.NotificationEventsHandler -= ShowMessage;
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowMessage("pippo"); 
+        }
+    }
+
     /// <summary>
     /// Mostra un messaggio di notifica generale
     /// </summary>
     /// <param name="message"></param>
     public void ShowMessage(string message)
     {
-        typewriter.ClearText(); // Puliamo il testo prima di iniziare
+        //typewriter.ClearText(); // Puliamo il testo prima di iniziare
 
-        StopAllCoroutines();
-        StartCoroutine(DialogueSequence(message));
+        //StopAllCoroutines();
+        Vfx_Typewriter scriptTypeWrite = ShowOnPanel();
+        StartCoroutine(DialogueSequence(message, scriptTypeWrite));
     }
 
     /// <summary>
@@ -33,13 +44,14 @@ public class Notification_Manager : MonoBehaviour
     /// <param name="argomento"></param>
     public void ShowNotificationCodexUpdate(string argomento)
     {
-        typewriter.ClearText(); // Puliamo il testo prima di iniziare
+        //typewriter.ClearText(); // Puliamo il testo prima di iniziare
 
         StopAllCoroutines();
-        StartCoroutine(DialogueSequence("Codex aggiornato: " + argomento));
+        Vfx_Typewriter scriptTypeWrite =  ShowOnPanel();
+        StartCoroutine(DialogueSequence("Codex aggiornato: " + argomento, scriptTypeWrite));
     }
 
-    private IEnumerator DialogueSequence(string message)  // Sequenza completa per mostrare un messaggio di dialogo
+    private IEnumerator DialogueSequence(string message, Vfx_Typewriter typewriter)  // Sequenza completa per mostrare un messaggio di dialogo
     {
         // 3. Ora che la box è aperta e il testo è attivo, scrivi!
         yield return StartCoroutine(typewriter.TypeText(message));
@@ -47,5 +59,12 @@ public class Notification_Manager : MonoBehaviour
         // 5. Chiudi
         yield return new WaitForSeconds(2f);
         typewriter.ClearText(); // Puliamo il testo dopo la visualizzazione
+    }
+
+    private Vfx_Typewriter ShowOnPanel()
+    {
+        GameObject newText = Instantiate(prefabTxtNotification, notifPanel.transform);
+        Destroy(newText, 3f);
+        return newText.GetComponent<Vfx_Typewriter>();
     }
 }
