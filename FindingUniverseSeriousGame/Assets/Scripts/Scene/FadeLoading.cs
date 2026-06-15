de551f280691f16cefd7d4a533a014fe83383aa7
StefanoCadalt <stefano.cadalt@gmail.com>
2026-06-15 19:12:29 +0200

    Aggiunto modello 3D della zona interna del secondo livello e aggiunta transizione dal livello 2 al livello 1

--- FindingUniverseSeriousGame/Assets/Scripts/Scene/FadeLoading.cs
+++ FindingUniverseSeriousGame/Assets/Scripts/Scene/FadeLoading.cs
@@ -7,6 +7,7 @@ public class FadeLoading : MonoBehaviour
     [SerializeField] private Image FadeToBlack;
     [SerializeField] private float fadeDuration = 1f;
     [SerializeField] private float fadeStop = 3f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,10 +25,18 @@ public class FadeLoading : MonoBehaviour
 
         while (elapsedTime < fadeDuration) // Durata dell'animazione (metà del tempo totale della notifica)
         {
+            // L'USCITA D'EMERGENZA: Se l'immagine è stata distrutta dal cambio scena, interrompi la Coroutine
+            if (FadeToBlack == null) yield break;
+
             FadeToBlack.color = new Color(0f, 0f, 0f, Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration));
             elapsedTime += Time.deltaTime;
             yield return null; // Attende il prossimo frame prima di continuare l'animazione
         }
-        DelegateClass.StartFirstMissionEventHandler.Invoke();
+        
+        // Evitiamo di lanciare l'evento se stiamo cambiando livello e l'oggetto non esiste più
+        if (FadeToBlack != null)
+        {
+            DelegateClass.StartFirstMissionEventHandler.Invoke();
+        }
     }
 }
