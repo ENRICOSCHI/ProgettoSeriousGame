484bf0823639a0db2a3d73ab81aabb73e9ac41bc
ENRICOSCHI <113851247+ENRICOSCHI@users.noreply.github.com>
2026-07-08 17:51:43 +0200

    Update VentoSolareEvento.cs
    Aggiunti commenti

--- FindingUniverseSeriousGame/Assets/Scripts/Events/VentoSolareEvento.cs
+++ FindingUniverseSeriousGame/Assets/Scripts/Events/VentoSolareEvento.cs
@@ -40,14 +40,19 @@ public class VentoSolareEvento : Eventi
 
     IEnumerator AttivaEvento(int secondiDelayEvento)
     {
+        // aspetto un tempo di delay prima di attivare il prossimo evento solare
         yield return new WaitForSeconds(secondiDelayEvento);
+
+        //se l'evento non è ancora stato scoperto...
         if (!ManagerHandler.ManagerInstance.CodexManager.categoryLists[1].entries[1].isDiscovered)
         {
-            ActiveSubtitlesWithAudio();
-            UnlockOnCodexMenu();
-            PersistentSceneData.Instance.isDescriptionVentoSolareHappened = true;
+            ActiveSubtitlesWithAudio();//attivo descrizione audio con sottotitoli
+            UnlockOnCodexMenu(); //sblocco largomento nel codex
+            PersistentSceneData.Instance.isDescriptionVentoSolareHappened = true; 
         }
-        NotificaPersonalizzata(notificaMessaggio[0]);
+
+        NotificaPersonalizzata(notificaMessaggio[0]); //notifico l'evento al giocatore
+        //Avviso l'UI tramite delegate che l'evento solare è attivo e far partire quindi "l'intereferenza" visiva
         DelegateClass.VentoSolareEventsHandler?.Invoke(true);
 
         if (tempestaSFX != null)
@@ -55,7 +60,9 @@ public class VentoSolareEvento : Eventi
         else
             Debug.LogWarning("Manca tempestaSFX in VentoSolareEvento.cs");
 
+        //aspetto la fine dell'evento
         yield return new WaitForSeconds(durataEvento);
+        //disattivo effetti alla UI
         DelegateClass.VentoSolareEventsHandler?.Invoke(false);
     }
 
