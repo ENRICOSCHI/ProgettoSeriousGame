cf2febd5850085f85412be6ae195ecda33b4542c
ENRICOSCHI <113851247+ENRICOSCHI@users.noreply.github.com>
2026-07-08 18:01:55 +0200

    Update FiondaEvento.cs
    Aggiunti commenti

--- FindingUniverseSeriousGame/Assets/Scripts/Events/FiondaEvento.cs
+++ FindingUniverseSeriousGame/Assets/Scripts/Events/FiondaEvento.cs
@@ -7,13 +7,15 @@ public class FiondaEvento : Eventi
         if (other.CompareTag("Player"))
         {
             NotificaPersonalizzata(notificaMessaggio[0]);
+            //se l'evento non è ancora stato scoperto...
             if (!PersistentSceneData.Instance.isDescriptionFiondaHappened)
             {
-                ActiveSubtitlesWithAudio();
-                UnlockOnCodexMenu();
+                ActiveSubtitlesWithAudio(); //attivo descrizione audio con sottotitoli
+                UnlockOnCodexMenu(); //sblocco argomento nel codex
                 PersistentSceneData.Instance.isDescriptionFiondaHappened = true;
             }
-            StartCoroutine(other.GetComponent<MovimentoNavicella>().AddBoost());
+            StartCoroutine(other.GetComponent<MovimentoNavicella>().AddBoost());//attivo il boost di velocità per la navicella
+            //aggiorno la velocità nella UI
             ManagerHandler.ManagerInstance.SpeedManager.UpdateSpeedDisplay(other.GetComponent<MovimentoNavicella>());
         }
     }
